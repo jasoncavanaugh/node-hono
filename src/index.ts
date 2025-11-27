b9dda43a118807e6271d2436d2ae436eee2e338a
Jason Cavanaugh <jascav418@gmail.com>
2025-11-27 09:15:53 -0500

    env stuff

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server";
+import { Hono } from "hono";
+import { env } from "./env.ts";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+const hostname = env.RAILWAY_PUBLIC_DOMAIN ?? "localhost";
+
+serve(
+  {
+    fetch: app.fetch,
+    port: env.PORT ?? 8080,
+    hostname,
+  },
+  (info) => {
+    console.log(`Server is running on ${hostname}:${info.port}`);
+  }
+);
